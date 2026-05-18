31f3ffc2b02363b4b8b92c7554871860ab457b25
Prateek Singh <prateek1032002@gmail.com>
2026-05-18 22:28:38 +0530

    Added binary search problems

---
+++ lowerBound.py
@@ -0,0 +1,27 @@
+def lowerBound(arr,target):
+    n=len(arr)
+    s=0
+    e=n-1
+    
+    ans=n
+    
+    while s<=e:
+        mid=s+(e-s)//2
+        
+        if arr[mid]>=target:
+            
+            ans=mid
+            e=mid-1
+        else:
+            s=mid+1
+            
+    return ans
+
+arr=list(map(int,input().split( )))
+target=int(input())
+
+result = lowerBound(arr,target)
+
+print("The lower bound is :",result)
+            
+        
